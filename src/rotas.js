edc61b1c0c0cb4b6256214b466ce9edf4e5a1f93
ThiagoSFerraz <oniferraz@gmail.com>
2020-12-03 22:12:30 -0300

    Criando as rotas para pedidos e produtos

--- src/rotas.js
+++ src/rotas.js
@@ -1,1 +1,13 @@
 import { Switch, Route } from 'react-router-dom';
+
+import { Produtos } from './pages/produtos'
+import { Pedidos } from './pages/pedidos'
+
+function Rotas() {
+    return (
+        <Switch>
+            <Route exact path="/" component={Produtos} />
+            <Route exact path="/pedidos" component={Pedidos} />
+        </Switch>
+    )
+}
